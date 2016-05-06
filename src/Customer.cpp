3da3e67c9c90c9015ba72d91584ddb4e948cf5dd
km121zips <mitroff@Kriss-MacBook.local>
2016-05-06 13:36:52 -0400

    Removed old code, now calling a new method

--- src/Customer.cpp
+++ src/Customer.cpp
@@ -52,7 +52,7 @@ double Customer::amountFor(const Rental& r) const {
             break;
         }
 
-	return 0;
+	return thisAmount;
 }
 
 // customer rental statement
@@ -81,25 +81,10 @@ std::string Customer::statement() const {
         result += it->getVideo().getTitle();
         result += "\t";
 
+	
         double thisAmount = 0;
-        switch(it->getVideo().getCode()) {
+	thisAmount = amountFor(*it);
 
-            case Video::REGULAR:
-            thisAmount += 2;
-            if (it->getDaysRented() > 2)
-                thisAmount += (it->getDaysRented() - 2) * 1.5;
-            break;
-
-            case Video::NEW_RELEASE:
-            thisAmount += it->getDaysRented() * 3;
-            break;
-
-            case Video::CHILDRENS:
-            thisAmount += 1.5;
-            if (it->getDaysRented() > 3)
-                thisAmount += (it->getDaysRented() - 3) * 1.5;
-            break;
-        }
 
         // amount of rental
         std::ostringstream out_str_stream;
